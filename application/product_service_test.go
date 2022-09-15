a16900e371c3cc1a6467aa0633ad80bc421d07d9
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-15 18:37:30 -0300

    feat: added test ProductServiceCreate with mocks

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -16,12 +16,27 @@ func TestProductService_Get(t *testing.T) {
 	product := mock_application.NewMockProductInterface(ctrl)
 	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
 	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
-
 	service := application.ProductService{
 		Persistence: persistence,
 	}
 
 	result, err := service.Get("123456789")
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
+
+func TestProductService_Create(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Create("Product 1", 10)
 	require.Nil(t, err)
 	require.Equal(t, product, result)
 }
