79898d2a747aa45910863b7aa60bfa2911db7940
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-15 18:08:40 -0300

    feat: added test ProductServiceGet with mocks

---
+++ application/product_service_test.go
@@ -0,0 +1,27 @@
+package application_test
+
+import (
+	"github.com/golang/mock/gomock"
+	"github.com/moreirak14/fullcycle-arquitetura-hexagonal/application"
+	mock_application "github.com/moreirak14/fullcycle-arquitetura-hexagonal/application/mocks"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestProductService_Get(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Get("123456789")
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
