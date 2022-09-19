33fac0149e2ddda13d4bb63a0d53b53b6323a218
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-19 15:00:00 -0300

    feat: added new router and function createProduct

--- adapters/web/handler/product.go
+++ adapters/web/handler/product.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
+	"github.com/moreirak14/fullcycle-arquitetura-hexagonal/adapters/dto"
 	"github.com/moreirak14/fullcycle-arquitetura-hexagonal/application"
 	"net/http"
 )
@@ -14,6 +15,7 @@ func MakeProductHandlers(
 	service application.ProductServiceInterface) {
 
 	router.Handle("/product/{id}", n.With(negroni.Wrap(getProduct(service)))).Methods("GET", "OPTIONS")
+	router.Handle("/product/", n.With(negroni.Wrap(createProduct(service)))).Methods("POST", "OPTIONS")
 }
 
 func getProduct(service application.ProductServiceInterface) http.Handler {
@@ -27,10 +29,35 @@ func getProduct(service application.ProductServiceInterface) http.Handler {
 			writer.WriteHeader(http.StatusNotFound)
 			return
 		}
-
 		err = json.NewEncoder(writer).Encode(product)
 		if err != nil {
 			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
+}
+
+func createProduct(service application.ProductServiceInterface) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Content-Type", "application/json")
+		var productDto dto.Product
+
+		err := json.NewDecoder(request.Body).Decode(&productDto)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			writer.Write(jsonError(err.Error()))
+			return
+		}
+		product, err := service.Create(productDto.Name, productDto.Price)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			writer.Write(jsonError(err.Error()))
+			return
+		}
+		err = json.NewEncoder(writer).Encode(product)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			writer.Write(jsonError(err.Error()))
 			return
 		}
 	})
